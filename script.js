1790a0b0bd7c1db7eb449f5a3eae3985f94358da
mark-s-bit <markchege003@gmail.com>
2026-01-20 08:20:28 +0300

    addjs

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const taskInput = document.getElementById("task-input");
+  const addTskbtn = document.getElementById("add-tsk-btn");
+  const taskListed = document.getElementById("task-listed");
 
-document.addEventListener('DOMContentLoaded',() => {
-     const taskInput = document.getElementById("task-input")
-     
-
+  const addTask = (event) => {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
+    if (!taskText) {
+      return;
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <input type = "checkbox" class="checkbox" 
+     >`
+    li.textContent = taskText;
+    taskListed.appendChild(li);
+    taskInput.value = "";
+  };
+  addTskbtn.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      addTask(e);
+    }
+  });
 });
